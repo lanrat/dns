9519242da917f461561f0796c29cd1f5dd59bac5
Ian Foster <ian@vorsk.com>
2020-05-18 12:20:34 -0700

    cleanup scan_Err

--- scan_err.go
+++ scan_err.go
@@ -1,23 +1,17 @@
 package dns
 
-import "log"
-
-func (zp *ZoneParser) ResetErr() error {
+func (zp *ZoneParser) ResetErr() ([]string, error) {
+	tokens := make([]string, 0)
 	if zp.parseErr != nil {
 		nextLine := false
-		// TODO testing keeping a record of all skipped tokens
-		tokens := make([]string, 0)
 		for !nextLine {
 			l, _ := zp.c.Next()
-			//log.Printf("miekg reset slurp: %+v\n", l.token)
 			tokens = append(tokens, l.token)
 			if l.value == zNewline || l.value == zEOF {
 				nextLine = true
 			}
 		}
 		zp.parseErr = nil
-		// TODO return the line?
-		log.Printf("Skipped line: %v", tokens)
 	}
-	return nil
+	return tokens, nil
 }
